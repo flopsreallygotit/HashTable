79ea3c8f97081e32b9b08b95f4e90286e169a817
flopsreallygotit <flopsreallygotit@gmail.com>
2023-04-14 12:20:27 +0300

    Tried to make List better.

--- HashTable/main.cpp
+++ HashTable/main.cpp
@@ -4,6 +4,15 @@
 
 int main ()
 {
+    list *List = listConstructor();
+
+    char *element = NULL;
+    scanf("%ms", &element);
+
+    list *pointerToNodeWithMyElement = insertAfter(List, element);
+    listDump(List);
+
+    listDestructor(List);
 
     return 0;
 }
